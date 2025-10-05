b58d9a24b5d008242c7a4155711e9c24b970cad3
Sophea Kong <sopheakongs001@gmail.com>
2025-10-05 12:58:54 +0700

    fix bug

--- W0-review-array.cpp
+++ W0-review-array.cpp
@@ -10,32 +10,24 @@ void input(int * p_arr, int pos, int val, int &n, int size = 10){
     p_arr[pos] = val;
     n = (pos < n)? n: n+1;
 }
-void insert(int* ptr_arr, int pos,int val,int &n,int size = 10){
-    // array is full
+void insert(int* ptr_arr, int pos, int val, int &n, int size = 10){
     if (n >= size){
-        cout<<"Error!"<<endl;
+        cout << "Error! Array is full." << endl;
         return;
     }
-    // position is negative number
-    if (pos < 0){
-        cout<<"Position can not be a negative number."<<endl;
+    if (pos < 0 || pos > n){
+        cout << "Invalid position." << endl;
         return;
     }
 
-    int temp[size];
-    int j = 0;
-    for (int i =pos;i<n;i++){
-        temp[j] = ptr_arr[i];
-        j++;
+    for (int i = n; i > pos; i--) {
+        ptr_arr[i] = ptr_arr[i - 1];
     }
+
     ptr_arr[pos] = val;
-    j=0;
-    for (int i=pos+1;i<n;i++){
-        ptr_arr[i] = temp[j];
-        j++;
-    }
     n++;
 }
+
 void deletes(int* ptr_arr, int pos,int &n,int size = 10){
     if (!ptr_arr){
         cout<<"array is empty"<<endl;
@@ -46,36 +38,22 @@ void deletes(int* ptr_arr, int pos,int &n,int size = 10){
         cout<<"index out of range"<<endl;
         return;
     }
-    int temp[size];
-    int j = 0;
-    for (int i =pos+1;i<n;i++){
-        temp[j] = ptr_arr[i];
-        j++;
-    }
-    j=0;
-    for (int i=pos;i<n;i++){
-        ptr_arr[i] = temp[j];
-        j++;
+    ptr_arr[pos] = 0;
+    for (int i =pos;i<n;i++){
+        ptr_arr[i] = ptr_arr[i+1];
     }
     n--;
-    ptr_arr[n] = 0;
 }
 int main(){
 
     int a[10] = {1,2};
     int n = 2;
-
-    input(a, 2, 10, n);
-    input(a, 3, 15, n);
-    input(a, 1, 25, n);
-    input(a, 4, 6, n);
-    insert(a , 0, -1,n);
-    deletes(a,0,n);
+    insert(a,0,1,n);
+    input(a,3,6,n);
     deletes(a,3,n);
     for(int i = 0; i < n; i++){
         cout<<"Value "<<i<<": "<<a[i]<<endl;
     }
-    
 
     return 0;
 }
